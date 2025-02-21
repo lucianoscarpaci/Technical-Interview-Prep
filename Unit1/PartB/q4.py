49ef9dd208c010470243ad7bd922c0e2198ae8da
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-02-20 22:11:17 -0500

    Update q4.py

--- Unit1/PartB/q4.py
+++ Unit1/PartB/q4.py
@@ -1,14 +1,14 @@
-def count_digits(n):
-    count = 0
-    while n > 0:
-        count += 1
-        n = n // 10
-        if n == 0:
-            count = 1
-    print(count)
-    return count
+def goldilocks_approved(nums):
+    for num in nums:
+        if num != min(nums) and max(nums):
+            return num
+        return -1
 
-n = 964
-count_digits(n)
-n = 0
-count_digits(n)
+nums = [3, 2, 1, 4]
+print(goldilocks_approved(nums))
+
+nums = [1, 2]
+print(goldilocks_approved(nums))
+
+nums = [2, 1, 3]
+print(goldilocks_approved(nums))
