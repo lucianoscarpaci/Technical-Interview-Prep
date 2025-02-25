9a54c5239931094ec071388def554d0cccbc69b1
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-02-25 15:34:09 -0500

    Finish Quiz

---
+++ Unit2/PartA/quiz.py
@@ -0,0 +1,13 @@
+def get_top_player(dictionary):
+    high_score = float('-inf')
+    top_player = ""
+
+    for name, score in dictionary.items():
+        if score > high_score:
+            high_score = score
+            top_player = name
+        
+    return top_player, high_score
+
+
+    
