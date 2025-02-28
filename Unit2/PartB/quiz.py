9d3a471d863654cc9b6e667a0961136e8e9bd17b
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-02-27 23:47:45 -0500

    Finish Part B

--- Unit2/PartB/quiz.py
+++ Unit2/PartB/quiz.py
@@ -1,3 +1,12 @@
+def count_words(sentence):
+    words = sentence.split() #['this', 'is', 'a', 'test', 'only']
+    word_count = {}
+    for word in words: # 'this'
+        word_count[word] = words.count(word)
+    return word_count
+# I dont understand why the output is not as expected
+# The expected output is {'this': 1, 'is': 1, 'a': 1, 'test': 1, 'only': 1}
 
 
-
+sentence = "this is a test only"
+print(count_words(sentence))
