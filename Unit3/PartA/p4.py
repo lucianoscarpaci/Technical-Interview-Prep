d7b30fc7570c6a51bbd1bc78888e8b0772a5e4dc
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-02 14:45:59 -0500

    Error 16 replaced 100

--- Unit3/PartA/p4.py
+++ Unit3/PartA/p4.py
@@ -11,12 +11,23 @@ def engagement_boost(engagements):
     
     squared_engagements.sort(reverse=True)#[100,4], [16,0], [9,3], [1,1], [0,2]
     
+    left = 0 #represents l
+    right = len(engagements) - 1 #4 #represents r
+    position = right
+    #for square, original_index in squared_engagements:
+    #    result[position] = square #[0,0,0,0,100], [0,0,0,16,100], [0,0,9,16,100], [0,1,9,16,100], [0,1,9,16,100]
+    #    position -= 1 #3, 2, 1, 0, -1
     result = [0] * len(engagements) #[0,0,0,0,0]
-    position = len(engagements) - 1 #4
-    
-    for square, original_index in squared_engagements:
-        result[position] = square #[0,0,0,0,100], [0,0,0,16,100], [0,0,9,16,100], [0,1,9,16,100], [0,1,9,16,100]
-        position -= 1 #3, 2, 1, 0, -1
+    while left <= right:
+        left_square = engagements[left] * engagements[left] #16
+        right_square = engagements[right] * engagements[right]
+        
+        if left_square > right_square: # error 16, replaced 100
+            result[position] = left_square
+            left += 1
+        else:
+            result[position] = right_square
+            right -= 1
     
     return result #[0,1,9,16,100]
 
