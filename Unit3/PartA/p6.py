80945a67546fd5fa7efcc62a3770c1f9a0cf1d50
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-03 00:28:24 -0500

    Fix P6
    Fix incorrect logic

--- Unit3/PartA/p6.py
+++ Unit3/PartA/p6.py
@@ -3,17 +3,20 @@
 from collections import deque
 
 def edit_post(post):
+    q = deque(post)
+    current_word = deque()
     reversed_post = []
-    for char in post:
-        q = deque(post)
-        q.reverse()
-        q.appendleft(q.pop())
-        q.rotate(-1)
-        q = list(q)
-    reversed_post.append(''.join(q))
-    return ' '.join(reversed_post)
 
-print(edit_post("this porsche"))
-print(edit_post("is a beast"))
+    for char in q:
+        if char != ' ':  # Continue reversing if not a space
+            current_word.appendleft(char) # add char to the left of the current word
+        else:
+            # When a space is detected, finalize the current word
+            reversed_post.append(''.join(current_word))
+            reversed_post.append(' ')  # Add the space back
+            current_word.clear() # clear the current word
+    reversed_post.append(''.join(current_word)) # add the last word
+    return ''.join(reversed_post)
+
 print(edit_post("Boost your engagement with these tips")) 
 print(edit_post("Check out my latest vlog"))
