913848e68d3a2fd7c7bf637f22c79b9d3a63d705
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-11 16:47:34 -0400

    Finished P2

--- Unit4/PartB/p2.py
+++ Unit4/PartB/p2.py
@@ -2,24 +2,25 @@
 # Write a function to find the smallest gap between any two consecutive work sessions.
 # The gap is measured in minutes.
 def find_smallest_gap(work_sessions):
+    work_sessions.sort()
     # Sort the work sessions by start time
     n = len(work_sessions)
     if n < 2:
         return 0
     
-    work_sessions.sort()
-
-    output = 0
+    smallest_gap = float("inf")
 
     for i in range(1, n):
-        output = abs(work_sessions[i-1][1] - work_sessions[i][0])
-    return output
-    
+        current_gap = work_sessions[i][0] - work_sessions[i - 1][1]
+        if current_gap < smallest_gap:
+            smallest_gap = current_gap
+    return smallest_gap
+
 work_sessions = [(900, 1100), (1300, 1500), (1600, 1800)]
 print(find_smallest_gap(work_sessions)) # [100]
 
 work_sessions_2 = [(1000, 1130), (1200, 1300), (1400, 1500)]
-print(find_smallest_gap(work_sessions_2)) # [100]
+print(find_smallest_gap(work_sessions_2)) # [70]
 
 work_sessions_3 = [(900, 1100), (1115, 1300), (1315, 1500)]
 print(find_smallest_gap(work_sessions_3)) # [15]
