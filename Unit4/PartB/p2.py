208321ac910381122137faec71b4fd6f68b266fb
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-11 16:25:36 -0400

    Started P3

--- Unit4/PartB/p2.py
+++ Unit4/PartB/p2.py
@@ -3,26 +3,23 @@
 # The gap is measured in minutes.
 def find_smallest_gap(work_sessions):
     # Sort the work sessions by start time
+    n = len(work_sessions)
+    if n < 2:
+        return 0
+    
     work_sessions.sort()
-    print(work_sessions)
-    # Set the smallest gap to the difference between the first two work sessions
-    difference = work_sessions[1][0] - work_sessions[0][1]
-    smallest_gap = difference  
-    # Loop through the work sessions starting from the second one
-    for i in range(1, len(work_sessions)):
-        # If the gap is smaller than the smallest gap, update the smallest gap
-        if difference < smallest_gap:
-            smallest_gap = difference
-        # Calculate the difference between the current work session and the next one
-        difference = work_sessions[i-1][0] - work_sessions[i][0]
-    # Return the smallest gap
-    return smallest_gap
 
+    output = 0
+
+    for i in range(1, n):
+        output = abs(work_sessions[i-1][1] - work_sessions[i][0])
+    return output
+    
 work_sessions = [(900, 1100), (1300, 1500), (1600, 1800)]
 print(find_smallest_gap(work_sessions)) # [100]
 
 work_sessions_2 = [(1000, 1130), (1200, 1300), (1400, 1500)]
-print(find_smallest_gap(work_sessions_2)) # [70]
+print(find_smallest_gap(work_sessions_2)) # [100]
 
 work_sessions_3 = [(900, 1100), (1115, 1300), (1315, 1500)]
 print(find_smallest_gap(work_sessions_3)) # [15]
---
+++ Unit4/PartB/p3.py
@@ -0,0 +1,16 @@
+def calculate_expenses(expenses):
+  pass
+
+
+
+expenses = [("Food", 12.5), ("Transport", 15.0), ("Accommodation", 50.0),
+            ("Food", 7.5), ("Transport", 10.0), ("Food", 10.0)]
+print(calculate_expenses(expenses))
+
+expenses_2 = [("Entertainment", 20.0), ("Food", 15.0), ("Transport", 10.0),
+              ("Entertainment", 5.0), ("Food", 25.0), ("Accommodation", 40.0)]
+print(calculate_expenses(expenses_2))
+
+expenses_3 = [("Utilities", 100.0), ("Food", 50.0), ("Transport", 75.0),
+              ("Utilities", 50.0), ("Food", 25.0)]
+print(calculate_expenses(expenses_3))
