940f47c151f74ad109de1c1b3e75dc68697e624d
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-16 15:26:06 -0400

    Update p2.py

--- Unit5/PartA/p2.py
+++ Unit5/PartA/p2.py
@@ -8,8 +8,9 @@ class Villager:
     def greet_player(self, player_name):
         return f"{self.name}: Hey there, {player_name}! How's it going, {self.catchphrase}!"
 
+
 bones = Villager("Bones", "Dog", "yip yip")
 print(bones.name)
-print(bones.species)  
-print(bones.catchphrase) 
-print(bones.furniture) 
+print(bones.species)
+print(bones.catchphrase)
+print(bones.furniture)
