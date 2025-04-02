ad27d1cc7238aa5049bb5605773cb70d34c2d54b
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-04-01 21:00:27 -0400

    Update p5.py

--- Unit7/PartA/p5.py
+++ Unit7/PartA/p5.py
@@ -1,10 +1,11 @@
 def power_of_four(n):
+    # This function calculates 4 raised to the power of n using recursion.
     if n == 0:
         return 1
     elif n > 0:
         return 4 * power_of_four(n - 1)
     else:
-        return 1 / (4 * power_of_four(-n - 1))
+        return (1 / 4) * power_of_four(n + 1)
 
 
 print(power_of_four(2))
