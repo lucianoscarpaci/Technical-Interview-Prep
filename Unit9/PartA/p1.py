013a6c53585e2c7b466f6e3dc18144fcbc8f2a80
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-04-15 20:23:00 -0400

    Finished P1

---
+++ Unit9/PartA/p1.py
@@ -0,0 +1,75 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, quantity, left=None, right=None):
+        self.val = quantity
+        self.left = left
+        self.right = right
+
+
+def build_tree(values):
+    if not values:
+        return None
+
+    def get_key_value(item):
+        if isinstance(item, tuple):
+            return item[0], item[1]
+        else:
+            return None, item
+
+    key, value = get_key_value(values[0])
+    root = TreeNode(value, key)
+    queue = deque([root])
+    index = 1
+
+    while queue:
+        node = queue.popleft()
+        if index < len(values) and values[index] is not None:
+            left_key, left_value = get_key_value(values[index])
+            node.left = TreeNode(left_value, left_key)
+            queue.append(node.left)
+        index += 1
+        if index < len(values) and values[index] is not None:
+            right_key, right_value = get_key_value(values[index])
+            node.right = TreeNode(right_value, right_key)
+            queue.append(node.right)
+        index += 1
+
+    return root
+
+
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
+
+
+def merge_orders(order1, order2):
+    if not order1:
+        return order2
+    if not order2:
+        return order1
+    merged_order = TreeNode(order1.val + order2.val)
+    merged_order.left = merge_orders(order1.left, order2.left)
+    merged_order.right = merge_orders(order1.right, order2.right)
+    return merged_order
+
+
+cookies1 = [1, 3, 2, 5]
+cookies2 = [2, 1, 3, None, 4, None, 7]
+order1 = build_tree(cookies1)
+order2 = build_tree(cookies2)
+print_tree(merge_orders(order1, order2))
