b50a3b03a0161ef03ecc75940980a2f7baf0270a
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-04-17 15:24:17 -0400

    Finished P1

---
+++ Unit9/PartB/p1.py
@@ -0,0 +1,87 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+
+def build_tree(values):
+    if not values:
+        return None
+
+    def get_key_value(item):
+        if isinstance(item, tuple):
+            return item[0], item[1]
+        else:
+            return None, item
+
+    key, value = get_key_value(values[0])
+    root = TreeNode(value, key)
+    queue = deque([root])
+    index = 1
+
+    while queue:
+        node = queue.popleft()
+        if index < len(values) and values[index] is not None:
+            left_key, left_value = get_key_value(values[index])
+            node.left = TreeNode(left_value, left_key)
+            queue.append(node.left)
+        index += 1
+        if index < len(values) and values[index] is not None:
+            right_key, right_value = get_key_value(values[index])
+            node.right = TreeNode(right_value, right_key)
+            queue.append(node.right)
+        index += 1
+
+    return root
+
+
+def is_balanced(display):
+    def check_balance(node):
+        if not node:
+            return True, 0
+
+        left_balanced, left_height = check_balance(node.left)
+        right_balanced, right_height = check_balance(node.right)
+
+        balanced = (
+            left_balanced and right_balanced and abs(left_height - right_height) <= 1
+        )
+        height = max(left_height, right_height) + 1
+        return balanced, height
+
+    balanced, _ = check_balance(display)
+    return balanced
+
+
+"""
+      🎂
+     /  \
+   🥮   🍩
+       /  \  
+     🥖    🧁
+
+"""
+# Using build_tree() function included at top of page
+baked_goods = ["🎂", "🥮", "🍩", "🥖", "🧁"]
+display1 = build_tree(baked_goods)
+
+"""
+          🥖
+         /  \
+       🧁    🧁
+       /       \
+      🍪       🍪
+     /           \
+    🥐           🥐  
+
+"""
+baked_goods = ["🥖", "🧁", "🧁", "🍪", None, None, "🍪", "🥐", None, None, "🥐"]
+display2 = build_tree(baked_goods)
+
+
+print(is_balanced(display1))
+print(is_balanced(display2))
