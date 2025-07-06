feb25e906d56ac0c8022a15698e9eb6e446341ef
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-07-06 18:43:41 -0400

    Finished PartC

--- Unit6/PartC/quiz.py
+++ Unit6/PartC/quiz.py
@@ -1,18 +1,35 @@
 class ListNode:
-    def __init__(self, value=0, next=None):
-        self.value = value
+    def __init__(self, data=0, next=None):
+        self.data = data
         self.next = next
 
 
-def count_nodes(head, val):
-    count = 0
+def print_linked_list(head):
     current = head
-    while current.next:
-        if current == None:
-            break
-        if current.value == val:
-            count += 1
+    while current:
+        print(current.data, end=" -> " if current.next else "")
         current = current.next
-    if current.value == val:
-        count += 1
-    return count
+    print()
+
+
+def delete_node(head, key):
+    if head is None:
+        return None
+
+    if head.data == key:
+        return head.next
+
+    prev = None
+    current = head
+
+    while current:
+        if current.data == key:
+            prev.next = current.next
+            return head
+        prev = current
+        current = current.next
+    return head
+
+
+head = ListNode(1, ListNode(2, ListNode(4, ListNode(5))))
+print_linked_list(head)
