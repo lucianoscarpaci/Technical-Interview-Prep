09c33e534800d5c71b39deb7a669988840c5687d
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-07-14 16:43:45 -0400

    Finished P2

---
+++ Unit8/PartC/p2.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+
+def right_vine(root):
+    current = root
+    if current is None:
+        return []
+    return [current.val] + right_vine(current.right)
+
+
+"""
+        Root
+      /      \
+    Node1    Node2
+  /         /    \
+Leaf1    Leaf2  Leaf3
+"""
+ivy1 = TreeNode(
+    "Root",
+    TreeNode("Node1", TreeNode("Leaf1")),
+    TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")),
+)
+
+"""
+      Root
+      /  
+    Node1
+    /
+  Leaf1  
+"""
+ivy2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
+
+print(right_vine(ivy1))
+print(right_vine(ivy2))
