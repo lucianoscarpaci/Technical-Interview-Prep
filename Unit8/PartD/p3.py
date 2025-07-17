2cab92301f071bfc55d95508729fce03e0f271e9
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-07-17 16:15:26 -0400

    Finished P3

---
+++ Unit8/PartD/p3.py
@@ -0,0 +1,84 @@
+from collections import deque
+
+
+# Tree Node class
+class TreeNode:
+    def __init__(self, value, key=None, left=None, right=None):
+        self.key = key
+        self.val = value
+        self.left = left
+        self.right = right
+
+
+def build_tree(values):
+    if not values:
+        return None
+
+    def get_key_value(item):
+        if isinstance(item, tuple):
+            return item[0], item[1]
+        else:
+            return None, item
+
+    key, value = get_key_value(values[0])
+    root = TreeNode(value, key)
+    queue = deque([root])
+    index = 1
+
+    while queue:
+        node = queue.popleft()
+        if index < len(values) and values[index] is not None:
+            left_key, left_value = get_key_value(values[index])
+            node.left = TreeNode(left_value, left_key)
+            queue.append(node.left)
+        index += 1
+        if index < len(values) and values[index] is not None:
+            right_key, right_value = get_key_value(values[index])
+            node.right = TreeNode(right_value, right_key)
+            queue.append(node.right)
+        index += 1
+
+    return root
+
+
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
+
+
+def add_plant(collection, name):
+    current = collection
+    if current is None:
+        return TreeNode(name)
+    if name < current.val:
+        current.left = add_plant(current.left, name)
+    else:
+        current.right = add_plant(current.right, name)
+    return current
+
+
+"""
+            Money Tree
+        /              \
+Fiddle Leaf Fig    Snake Plant
+"""
+
+# Using build_tree() function at the top of page
+values = ["Money Tree", "Fiddle Leaf Fig", "Snake Plant"]
+collection = build_tree(values)
+
+# Using print_tree() function at the top of page
+print_tree(add_plant(collection, "Aloe"))
