ee347efc9c87f37dc09cdfd4494aa1627269e319
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-11-04 21:58:50 -0500

    Updated Files

--- Unit8/PartD/p3.py
+++ Unit8/PartD/p3.py
@@ -1,79 +1,28 @@
-from collections import deque
-
-
-# Tree Node class
 class TreeNode:
-    def __init__(self, value, key=None, left=None, right=None):
-        self.key = key
+    def __init__(self, value, left=None, right=None):
         self.val = value
         self.left = left
         self.right = right
 
-
-def build_tree(values):
-    if not values:
-        return None
-
-    def get_key_value(item):
-        if isinstance(item, tuple):
-            return item[0], item[1]
-        else:
-            return None, item
-
-    key, value = get_key_value(values[0])
-    root = TreeNode(value, key)
-    queue = deque([root])
-    index = 1
-
-    while queue:
-        node = queue.popleft()
-        if index < len(values) and values[index] is not None:
-            left_key, left_value = get_key_value(values[index])
-            node.left = TreeNode(left_value, left_key)
-            queue.append(node.left)
-        index += 1
-        if index < len(values) and values[index] is not None:
-            right_key, right_value = get_key_value(values[index])
-            node.right = TreeNode(right_value, right_key)
-            queue.append(node.right)
-        index += 1
-
-    return root
-
-
-def print_tree(root):
-    if not root:
-        return "Empty"
-    result = []
-    queue = deque([root])
-    while queue:
-        node = queue.popleft()
-        if node:
-            result.append(node.val)
-            queue.append(node.left)
-            queue.append(node.right)
-        else:
-            result.append(None)
-    while result and result[-1] is None:
-        result.pop()
-    print(result)
-
-
-def add_plant(collection, name):
-    collection = plant
-    if collection == name:
-        do something
-    return collection
+def get_decision(root):
+    if root.val == "AND":
+        return get_decision(root.left) and get_decision(root.right)
+    elif root.val == "OR":
+        return get_decision(root.left) or get_decision(root.right)
+    else:
+        return root.val
 
 """
-            Money Tree
-        /              \
-Fiddle Leaf Fig    Snake Plant
+        OR
+      /    \
+    True  False
 """
+expression1 = TreeNode("OR", TreeNode(True), TreeNode(False))
 
-# Using build_tree() function at the top of page
-values = ["Money Tree", "Fiddle Leaf Fig", "Snake Plant"]
-collection = build_tree(values)
+"""
+       False
+"""
+expression2 = TreeNode(False)
 
-# Using print_tree() function at the top of page
-print_tree(add_plant(collection, "Aloe"))
+print(get_decision(expression1))
+print(get_decision(expression2))
---
+++ Unit8/PartD/p4.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def leftmost_path(root):
+    path = []
+    current = root
+    while current:
+        path.append(current.val)
+        current = current.left
+    return path
+
+"""
+        CaveA
+       /      \
+    CaveB    CaveC
+    /   \        \
+CaveD CaveE     CaveF  
+"""
+system_a = TreeNode("CaveA", 
+                  TreeNode("CaveB", TreeNode("CaveD"), TreeNode("CaveE")), 
+                          TreeNode("CaveC", None, TreeNode("CaveF")))
+
+"""
+  CaveA
+      \
+      CaveB
+        \
+        CaveC  
+"""
+system_b = TreeNode("CaveA", None, TreeNode("CaveB", None, TreeNode("CaveC")))
+
+print(leftmost_path(system_a))
+print(leftmost_path(system_b))
