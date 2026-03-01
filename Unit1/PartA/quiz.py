487859a252dd67c67accf0a567950f61b68e81a3
Luciano Scarpaci <tauxsik@gmail.com>
2026-03-01 14:18:24 -0500

    Unit1 (#2)
    * Finish Unit1
    
    * Add reverse list problem
    
    ---------
    
    Co-authored-by: Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>

--- Unit1/PartA/quiz.py
+++ Unit1/PartA/quiz.py
@@ -1,18 +1,10 @@
-def tiggerfy(s):
-    ans = ""
-    for c in s:
-        if c in "tigger":
-            continue
-        ans += c
-    return ans
+def reverse_lst(lst):
+    left = 0
+    right = len(lst) - 1
 
-
-s = "suspicerous"
-print(tiggerfy(s))
-
-s = "Trigger"
-print(tiggerfy(s))
-tiggerfy(s)
-s = "Hunny"
-print(tiggerfy(s))
-tiggerfy(s)
+    when left <= right:
+        mid = (left + right) // 2
+        lst[left], lst[right] = lst[right], lst[left]
+        left += 1
+        right -= 1
+    return lst
