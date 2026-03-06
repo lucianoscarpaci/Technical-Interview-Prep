935efa3274a3caa60969c99f0f72bd3009bedaa9
Luciano Scarpaci <tauxsik@gmail.com>
2026-03-06 13:29:06 -0500

    Unit2 (#4)
    * Finish Unit1
    
    * Add reverse list problem
    
    * Learn the coin segments problem
    
    * Solved p2.py
    
    * Solved p4.py
    
    * Finished quiz.py
    
    ---------
    
    Co-authored-by: Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>

---
+++ Unit2/PartC/quiz.py
@@ -0,0 +1,16 @@
+princess_movies = {
+    "snow white": 1937,
+    "cinderella": 1950,
+    "sleeping beauty": 1959,
+    "the little mermaid": 1989,
+    "beauty and the beast": 1991,
+    "aladdin": 1992,
+    "pocahontas": 1995,
+    "mulan": 1998,
+    "tarzan": 1999,
+    "the princess and the frog": 2009,
+    "tangled": 2010,
+    "brave": 2012,
+    "frozen": 2013,
+}
+print(princess_movies.items()[0])  # 1950
