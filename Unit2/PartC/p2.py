7c03ec99d309903a07beb311c20077942ee031ca
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2026-03-05 21:33:07 -0500

    Solved p2.py

--- Unit2/PartC/p2.py
+++ Unit2/PartC/p2.py
@@ -1,27 +1,28 @@
-def word_pattern(pattern, s):
-    words = s.split()
-    if len(pattern) != len(words):
-        return False
-    char_to_word = {}
-    word_to_char = {}
-    for char, word in zip(pattern, words):
-        if char in char_to_word:
-            if char_to_word[char] != word:
-                return False
+# Understand: Find unique occourences of souvenirs in a list
+# and count them. For example, hat, hat, keychain, keychain, keychain, postcard would return 2 for hat, 3 for keychain and 1 for postcard.
+# Match: Hashmap or dictionary to store the souvenir as key and count as value.
+# Plan: Count all the souviners in a dictionary, iterate through souveirs, count them, and store them in a dictionary. Put them in a set and compare them in a length of set.
+# Implement: Use dictionary to count the souveirs, then use set to find unique count of the souviners.
+# Review: Inputs array of souv
+# Evaluate:
+
+
+def unique_souvenir_counts(souvenirs):
+    count_souviners = {}
+    for souvenir in souvenirs:
+        if souvenir in count_souviners:
+            count_souviners[souvenir] += 1
         else:
-            char_to_word[char] = word
+            count_souviners[souvenir] = 1
 
-        if word in word_to_char:
-            if word_to_char[word] != char:
-                return False
-        else:
-            word_to_char[word] = char
-    return True
+    visited_souviners = set(count_souviners.values())
+    return len(visited_souviners) == len(count_souviners)
 
 
-pattern = "abba"
-s = "dog cat cat dog"
-print(word_pattern(pattern, s))  # Output: True
-pattern = "abba"
-s = "dog cat cat fish"
-print(word_pattern(pattern, s))  # Output: False
+souvenirs1 = ["keychain", "hat", "hat", "keychain", "keychain", "postcard"]
+souvenirs2 = ["postcard", "postcard", "postcard", "postcard"]
+souvenirs3 = ["keychain", "magnet", "hat", "candy", "postcard", "stuffed bear"]
+
+print(unique_souvenir_counts(souvenirs1))
+print(unique_souvenir_counts(souvenirs2))
+print(unique_souvenir_counts(souvenirs3))
